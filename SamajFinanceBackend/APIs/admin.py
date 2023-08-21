1cd2c47e079e3995f4d4e3636c457d2d5b84a230
Anup-Agharkar <142709802+Anup-Agharkar@users.noreply.github.com>
2023-08-21 23:07:00 +0530

    initial commmit

---
+++ SamajFinanceBackend/APIs/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from django.contrib.auth import get_user_model
+from django.contrib import admin
+User = get_user_model()
+
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import PhoneOTP
+admin.site.register(PhoneOTP)
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('username', 'email', 'phone', 'is_staff', 'is_active',)
+    list_filter = ('username', 'email', 'phone', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username','email','phone', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.register(User, CustomUserAdmin)
+
